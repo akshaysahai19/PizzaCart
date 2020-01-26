8e25eef4798287268fe6d88236251d007a80136e
Akshay Sahai <akshay95sahai@gmail.com>
2020-01-27 02:25:54 +0530

    Commit ViewModel setup with Custom Dialog layout

--- app/src/main/java/com/test/pizzacart/MainActivity.java
+++ app/src/main/java/com/test/pizzacart/MainActivity.java
@@ -1,16 +1,42 @@
 package com.test.pizzacart;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
+import com.test.pizzacart.databinding.ActivityMainBinding;
+import com.test.pizzacart.model.PizzaInfo;
+import com.test.pizzacart.viewmodel.PizzaViewModel;
+
 public class MainActivity extends AppCompatActivity {
 
-    
+    ActivityMainBinding activityMainBinding;
+    PizzaViewModel pizzaViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        pizzaViewModel = ViewModelProviders.of(this).get(PizzaViewModel.class);
+        pizzaViewModel.getPizzas().observe(this, new Observer<PizzaInfo>() {
+            @Override
+            public void onChanged(@Nullable PizzaInfo pizzaInfo) {
+
+                System.out.println("pizzas - "+pizzaInfo);
+                if (pizzaInfo.isVeg()) {
+                    activityMainBinding.vegNonveg.setColorFilter(ContextCompat.getColor(MainActivity.this,
+                            R.color.veg), android.graphics.PorterDuff.Mode.SRC_IN);
+                } else {
+                    activityMainBinding.vegNonveg.setColorFilter(ContextCompat.getColor(MainActivity.this,
+                            R.color.nonveg), android.graphics.PorterDuff.Mode.SRC_IN);
+                }
+            }
+        });
     }
 }
